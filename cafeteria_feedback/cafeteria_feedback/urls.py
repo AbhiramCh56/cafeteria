215ad9af98c2807ea56545bcbf295afbe9ffd774
Abhiram <sriabhiram.ch@gmail.com>
2024-12-29 01:04:03 +0530

    Final

--- cafeteria_feedback/cafeteria_feedback/urls.py
+++ cafeteria_feedback/cafeteria_feedback/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('feedback.urls')),  # This will direct to your feedback app
+    path('', include('feedback.urls')),  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
